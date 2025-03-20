558298a0c9779e057357fa97eeea7b1124bcc3f5
killman2859 <78819909+killman2859@users.noreply.github.com>
2025-03-20 18:32:34 +0300

    init

--- data/jobs.py
+++ data/jobs.py
@@ -16,14 +16,9 @@ class Jobs(SqlAlchemyBase):
     collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     start_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
-    end_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                    default=datetime.datetime.now)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                 default=datetime.datetime.now)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
 
-
     user_team_leader = orm.relationship('User')
-
-
-
-
--- data/users.py
+++ data/users.py
@@ -20,9 +20,9 @@ class User(SqlAlchemyBase):
                               index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
-    #jobs = orm.relationship("Jobs", back_populates='user')
+                                      default=datetime.datetime.now)
+
+    # jobs = orm.relationship("Jobs", back_populates='user')
 
     def __repr__(self):
         return f'< Colonist > {self.id} {self.surname} {self.name}'
-
